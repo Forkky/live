08d21d00abb3b947f2922e913059d728cd53e978
jeraldhere <jeraldhere@gmail.com>
2015-04-22 15:14:12 -0400

    Coming soon page
    Coming soon page

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html> 
+<html dir="ltr" lang="en-US">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!-- Google Analytics tracking script -->
+
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42687170-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+        <!-- Title: Change the title and description to suit your needs. -->
+        <title>forkky.com</title>
+        <meta name="description" content="forkky.com coming soon.">
+
+        <!-- Viewport Meta: Just taming mobile devices like iPads and iPhones. -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"/>
+
+        <!-- Google Fonts: The default font for this template. -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+
+        <!-- Styles: The primary styles for this template. -->
+        <link rel="stylesheet" type="text/css" href="assets/styles/normalize.css">
+        <link rel="stylesheet" type="text/css" href="assets/styles/main.css">
+        <link rel="stylesheet" type="text/css" href="assets/styles/responsive.css">
+        <link rel="stylesheet" type="text/css" href="assets/styles/theme.css">
+
+        <!-- Favicon: Change to whatever you like within the "assets/images" folder. -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
+    </head>
+
+    <body>
+        <!-- Status: Shows if the subscription succeeded or not. -->
+        <div id="loading">
+            <span class="status icon"></span>
+        </div>
+        
+        <!-- Header: Your site logo, tagline and project status. -->
+        <header class="row" id="header">
+            <div class="content">
+                <!-- Logo & Tagline: Delete "class="logo"" to remove the logo or upload your own logo to "assets/images". -->
+                <span><br></span>
+            </div>
+
+            <!-- Status: Change the numbers below to reflect your project status. -->
+            <div class="status" style="width: 75%;">
+                <span>75%</span>
+            </div>
+        </header>
+
+        <!-- Intro: Your intro text and MailChimp form. -->
+        <div class="row" id="intro">
+            <div class="content">
+                <!-- Pre-Submission: This is what's displayed before the subscription form has been submitted. -->
+                <div id="pre-subscribe">
+                    <div class="row" id="copy">
+                        <h1>Something <strong>good</strong> <br> is coming <strong> soon</strong>.</h1>
+                        
+                    </div>
+
+                    
+                </div>
+
+            
+                
+
+                <!-- Social Links: Add or remove your social links here. -->
+                <div class="row" id="social">
+                    <a class="icon twitter" href="http://twitter.com/theforkky"></a>
+                    <!-- <a class="icon facebook" href="http://facebook.com/yourid"></a> -->
+                    <!-- <a class="icon email" href="mailto:info@forkky.com"></a> -->
+                </div>
+            </div>
+        </div>
+
+        <!-- Required Scripts: Not too much needed for Launch. -->
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+        <script src="assets/scripts/main.js"></script>
+    </body>
+</html>
